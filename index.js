c734b0873494d66f95109ad84bed846db56117b3
TatchumNono <Tatchumnono@gmail.com>
2021-10-01 11:31:52 +0200

    did some correction added some scripts

--- index.js
+++ index.js
@@ -19,7 +19,5 @@ function openPage(pageName, elmnt, color) {
   elmnt.style.backgroundColor = color;
 }
 
-// Get the element with id="defaultOpen" and click on it
+//Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-
-addEventListener('mouseleave')
